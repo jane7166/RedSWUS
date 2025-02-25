a2b4495f86133d4fba71280f2a3223b24b032c22
jane7166 <122887267+jane7166@users.noreply.github.com>
2025-02-25 15:55:35 +0900

    std modify

--- RedSWUS-flask/std_handlers.py
+++ RedSWUS-flask/std_handlers.py
@@ -7,12 +7,13 @@ from detectron2.engine import DefaultPredictor
 from detectron2.config import get_cfg
 from models import StdResult, db, FirstPreprocessingResult
 
-
-
 class DetectronHandler:
     def __init__(self):
+        # stdoutput 폴더가 없으면 생성
+        os.makedirs("./stdoutput", exist_ok=True)
+
         # Detectron2 설정 및 모델 초기화
-        torch.cuda.empty_cache() 
+        torch.cuda.empty_cache()
         self.cfg = get_cfg()
         self.cfg.merge_from_file("./config.yaml")
         self.cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5
@@ -24,7 +25,7 @@ class DetectronHandler:
         """
         STD 예측을 처리하는 메서드.
         """
-        torch.cuda.empty_cache() 
+        torch.cuda.empty_cache()
         first_result = FirstPreprocessingResult.query.filter_by(first_result_code=first_result_code).first()
         if not first_result:
             print("First preprocessing result not found.")
@@ -34,7 +35,7 @@ class DetectronHandler:
         if not os.path.exists(file_path):
             print("Failed to load the image.")
             return {"error": "File not found at the specified path."}, 404
-        try: 
+        try:
             with open(file_path, 'rb') as file:
                 np_img = np.frombuffer(file.read(), np.uint8)  # 파일 데이터를 NumPy 배열로 변환
 
@@ -43,16 +44,15 @@ class DetectronHandler:
             if img is None:
                 return {"error": "Failed to load the image for prediction."}, 400
 
-            # Detectron2 예측 실행
-            
+            # STD 예측 실행
             outputs = self.predictor(img)
             print("Prediction completed.")
             instances = outputs["instances"].to("cpu")
             boxes = instances.pred_boxes.tensor.numpy()
             classes = instances.pred_classes.numpy()
             scores = instances.scores.numpy()
-            # 바운딩 박스대로 이미지 크롭
 
+            # 바운딩 박스대로 이미지 크롭
             if boxes.size == 0:
                 return 0
 
@@ -75,7 +75,7 @@ class DetectronHandler:
                 temp_filename = temp_filename.replace('/tmp/', './stdoutput/')
                 cv2.imwrite(temp_filename, cropped_img)
 
-            std_result =  StdResult(
+            std_result = StdResult(
                 video_code=first_result.video_code,
                 first_result_code=first_result.first_result_code,
                 std_result_path=temp_filename
@@ -106,7 +106,6 @@ def run_all_handlers(first_result_list):
     """
     STD 예측 및 후속 처리를 실행하는 함수.
     """
-
     std_result_list = []
 
     for first_result_code in first_result_list:
